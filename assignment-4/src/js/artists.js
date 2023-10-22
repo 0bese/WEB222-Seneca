dc673c2d08ef01db973aaa831a474383a2d905c3
Kojo Anyane Obese <obesean@icloud.com>
2023-10-22 19:54:14 -0400

    added artiste to artist.js

---
+++ assignment-4/src/js/artists.js
@@ -0,0 +1,34 @@
+/**
+ * artists.js
+ *
+ * The app's list of Artists
+ */
+
+window.artists = [
+  /* TODO */
+  { 
+    artistId : "AID-0233",
+    name : "Sarkodie",
+    urls : [
+      {url : "https://instagram.com/sarkodie?igshid=MzRlODBiNWFlZA==", name : "Instagram"},
+      {url : "https://x.com/sarkodie?s=21&t=YedajTwsDR-XWX4wc3lc0w", name : "X"}
+    ]
+  },
+  { 
+    artistId : "AID-0331",
+    name : "BlackSherif",
+    urls : [
+      {url : "https://instagram.com/blacksherif_?igshid=MzRlODBiNWFlZA==", name : "Instagram"},
+      {url : "https://x.com/blacksherif_?s=21&t=YedajTwsDR-XWX4wc3lc0w", name : "X"}
+    ]
+  },
+  { 
+    artistId : "AID-0635",
+    name : "Kwesi Arthur",
+    urls : [
+      {url : "https://instagram.com/kwesiarthur_?igshid=MzRlODBiNWFlZA==", name : "Instagram"},
+      {url : "https://x.com/kwesiarthur_?s=21&t=YedajTwsDR-XWX4wc3lc0w", name : "X"}
+    ]
+  }
+
+];
---
+++ assignment-4/src/js/songs.js
@@ -0,0 +1,201 @@
+/**
+ * songs.js
+ *
+ * The app's songs
+ */
+
+window.songs = [
+  /* TODO */
+  //Sarkodie songs
+  {
+    songID: "SID-0011",
+    artistId : "AID-0233",
+    title : "Bossy",
+    year : "2016",
+    duration : 310,
+    url : "https://youtu.be/HiaMW7Vq_lw?si=hcnsGdxeDhUQJIwe",
+    explicit : true
+  },
+  {
+    songID: "SID-0022",
+    artistId : "AID-0233",
+    title : "Take it back",
+    year : "2016",
+    duration : 140,
+    url : "https://youtu.be/jxKox9MgDDE?si=9i8KIc25gzgHfQ-Y",
+    explicit : true
+  },
+  {
+    songID: "SID-0033",
+    artistId : "AID-0233",
+    title : "Better Days",
+    year : "2022",
+    duration : 244,
+    url : "https://youtu.be/nSktAGGjBas?si=_8R8iY67tO49rZuY",
+    explicit : true
+  },
+  {
+    songID: "SID-0044",
+    artistId : "AID-0233",
+    title : "Coachella",
+    year : "2021",
+    duration : 186,
+    url : "https://youtu.be/wW8BN2YPjEQ?si=WTEgFNU6yvfWMGOw",
+    explicit : true
+  },
+  {
+    songID: "SID-0055",
+    artistId : "AID-0233",
+    title : "Country Side",
+    year : "2023",
+    duration : 195,
+    url : "https://youtu.be/HVXaDZWF208?si=l8iWvnvb-2c_o4R7",
+    explicit : true
+  },
+  {
+    songID: "SID-0066",
+    artistId : "AID-0233",
+    title : "Confam",
+    year : "2023",
+    duration : 152,
+    url : "https://youtu.be/JJIMs9uUUFM?si=B5hqNUTfTYrp8Ryq",
+    explicit : true
+  },
+  {
+    songID: "SID-0077",
+    artistId : "AID-0233",
+    title : "Rollies and Cigars",
+    year : "2021",
+    duration : 213,
+    url : "https://youtu.be/DP2_6uTtUuU?si=2J-xT_eXEgW8fITv",
+    explicit : true
+  },
+  //BlackSheriff songs
+  {
+    songID: "SID-1111",
+    artistId : "AID-0331",
+    title : "Simmer Down",
+    year : "2023",
+    duration : 182,
+    url : "https://youtu.be/7a34tgFDonI?si=CeJS883yDt3TsL7U",
+    explicit : true
+  },
+  {
+    songID: "SID-1122",
+    artistId : "AID-0331",
+    title : "YAYA",
+    year : "2023",
+    duration : 199,
+    url : "https://youtu.be/r_s9rszKTBE?si=W0x9lD6n0psD2IfO",
+    explicit : true
+  },
+  {
+    songID: "SID-1133",
+    artistId : "AID-0331",
+    title : "Kwaku the Traveller",
+    year : "2022",
+    duration : 199,
+    url : "https://youtu.be/GIDiI5kyBDQ?si=IJj3i-mGpFXrjaGB",
+    explicit : true
+  },
+  {
+    songID: "SID-1144",
+    artistId : "AID-0331",
+    title : "Oil In My Head",
+    year : "2023",
+    duration : 183,
+    url : "https://youtu.be/uwojeKVV7Dk?si=-LWYQaGuqE4Ucz55",
+    explicit : true
+  },
+  {
+    songID: "SID-1155",
+    artistId : "AID-0331",
+    title : "45",
+    year : "2022",
+    duration : 200,
+    url : "https://youtu.be/hSib7E4Ctcg?si=heHqfVEOztfG2y4M",
+    explicit : true
+  },
+  {
+    songID: "SID-1166",
+    artistId : "AID-0331",
+    title : "Second Sermon",
+    year : "2022",
+    duration : 199,
+    url : "https://youtu.be/cPf8LT2uNbs?si=vkFTZjeCPwGy2eeO",
+    explicit : true
+  },
+  {
+    songID: "SID-1177",
+    artistId : "AID-0331",
+    title : "Konongo Zongo",
+    year : "2022",
+    duration : 202,
+    url : "https://youtu.be/BtJQX53Syec?si=sSDtNdj5jxecPq2n",
+    explicit : true
+  },
+  //Kwesi Arthur
+  {
+    songID: "SID-2211",
+    artistId : "AID-0635",
+    title : "",
+    year : "",
+    duration : ,
+    url : "",
+    explicit : true
+  },
+  {
+    songID: "SID-2222",
+    artistId : "AID-0635",
+    title : "",
+    year : "",
+    duration : ,
+    url : "",
+    explicit : true
+  },
+  {
+    songID: "SID-2233",
+    artistId : "AID-0635",
+    title : "",
+    year : "",
+    duration : ,
+    url : "",
+    explicit : true
+  },
+  {
+    songID: "SID-2244",
+    artistId : "AID-0635",
+    title : "",
+    year : "",
+    duration : ,
+    url : "",
+    explicit : true
+  },
+  {
+    songID: "SID-2255",
+    artistId : "AID-0635",
+    title : "",
+    year : "",
+    duration : ,
+    url : "",
+    explicit : true
+  },
+  {
+    songID: "SID-2266",
+    artistId : "AID-0635",
+    title : "",
+    year : "",
+    duration : ,
+    url : "",
+    explicit : true
+  },
+  {
+    songID: "SID-2277",
+    artistId : "AID-0635",
+    title : "",
+    year : "",
+    duration : ,
+    url : "",
+    explicit : true
+  },
+];
