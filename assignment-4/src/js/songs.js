c014092cef0c30581e567843cd7d8e2ada72d990
Kojo Anyane Obese <obesean@icloud.com>
2023-10-26 15:46:14 -0400

    Completed Assignment 4

--- assignment-4/src/js/songs.js
+++ assignment-4/src/js/songs.js
@@ -9,193 +9,193 @@ window.songs = [
   //Sarkodie songs
   {
     songID: "SID-0011",
-    artistId : "AID-0233",
-    title : "Bossy",
-    year : "2016",
-    duration : 316,
-    url : "https://youtu.be/HiaMW7Vq_lw?si=hcnsGdxeDhUQJIwe",
-    explicit : true
+    artistId: "AID-0233",
+    title: "Bossy",
+    year: "2016",
+    duration: 316,
+    url: "https://youtu.be/HiaMW7Vq_lw?si=hcnsGdxeDhUQJIwe",
+    explicit: true
   },
   {
     songID: "SID-0022",
-    artistId : "AID-0233",
-    title : "Take it back",
-    year : "2016",
-    duration : 154,
-    url : "https://youtu.be/jxKox9MgDDE?si=9i8KIc25gzgHfQ-Y",
-    explicit : true
+    artistId: "AID-0233",
+    title: "Take it back",
+    year: "2016",
+    duration: 154,
+    url: "https://youtu.be/jxKox9MgDDE?si=9i8KIc25gzgHfQ-Y",
+    explicit: true
   },
   {
     songID: "SID-0033",
-    artistId : "AID-0233",
-    title : "Better Days",
-    year : "2022",
-    duration : 246,
-    url : "https://youtu.be/nSktAGGjBas?si=_8R8iY67tO49rZuY",
-    explicit : true
+    artistId: "AID-0233",
+    title: "Better Days",
+    year: "2022",
+    duration: 246,
+    url: "https://youtu.be/nSktAGGjBas?si=_8R8iY67tO49rZuY",
+    explicit: true
   },
   {
     songID: "SID-0044",
-    artistId : "AID-0233",
-    title : "Coachella",
-    year : "2021",
-    duration : 190,
-    url : "https://youtu.be/wW8BN2YPjEQ?si=WTEgFNU6yvfWMGOw",
-    explicit : true
+    artistId: "AID-0233",
+    title: "Coachella",
+    year: "2021",
+    duration: 190,
+    url: "https://youtu.be/wW8BN2YPjEQ?si=WTEgFNU6yvfWMGOw",
+    explicit: true
   },
   {
     songID: "SID-0055",
-    artistId : "AID-0233",
-    title : "Country Side",
-    year : "2023",
-    duration : 205,
-    url : "https://youtu.be/HVXaDZWF208?si=l8iWvnvb-2c_o4R7",
-    explicit : true
+    artistId: "AID-0233",
+    title: "Country Side",
+    year: "2023",
+    duration: 205,
+    url: "https://youtu.be/HVXaDZWF208?si=l8iWvnvb-2c_o4R7",
+    explicit: true
   },
   {
     songID: "SID-0066",
-    artistId : "AID-0233",
-    title : "Confam",
-    year : "2023",
-    duration : 152,
-    url : "https://youtu.be/JJIMs9uUUFM?si=B5hqNUTfTYrp8Ryq",
-    explicit : true
+    artistId: "AID-0233",
+    title: "Confam",
+    year: "2023",
+    duration: 152,
+    url: "https://youtu.be/JJIMs9uUUFM?si=B5hqNUTfTYrp8Ryq",
+    explicit: true
   },
   {
     songID: "SID-0077",
-    artistId : "AID-0233",
-    title : "Rollies and Cigars",
-    year : "2021",
-    duration : 235,
-    url : "https://youtu.be/DP2_6uTtUuU?si=2J-xT_eXEgW8fITv",
-    explicit : true
+    artistId: "AID-0233",
+    title: "Rollies and Cigars",
+    year: "2021",
+    duration: 235,
+    url: "https://youtu.be/DP2_6uTtUuU?si=2J-xT_eXEgW8fITv",
+    explicit: true
   },
   //BlackSheriff songs
   {
     songID: "SID-1111",
-    artistId : "AID-0331",
-    title : "Simmer Down",
-    year : "2023",
-    duration : 181,
-    url : "https://youtu.be/7a34tgFDonI?si=CeJS883yDt3TsL7U",
-    explicit : true
+    artistId: "AID-0331",
+    title: "Simmer Down",
+    year: "2023",
+    duration: 181,
+    url: "https://youtu.be/7a34tgFDonI?si=CeJS883yDt3TsL7U",
+    explicit: true
   },
   {
     songID: "SID-1122",
-    artistId : "AID-0331",
-    title : "YAYA",
-    year : "2023",
-    duration : 199,
-    url : "https://youtu.be/r_s9rszKTBE?si=W0x9lD6n0psD2IfO",
-    explicit : true
+    artistId: "AID-0331",
+    title: "YAYA",
+    year: "2023",
+    duration: 199,
+    url: "https://youtu.be/r_s9rszKTBE?si=W0x9lD6n0psD2IfO",
+    explicit: true
   },
   {
     songID: "SID-1133",
-    artistId : "AID-0331",
-    title : "Kwaku the Traveller",
-    year : "2022",
-    duration : 212,
-    url : "https://youtu.be/GIDiI5kyBDQ?si=IJj3i-mGpFXrjaGB",
-    explicit : true
+    artistId: "AID-0331",
+    title: "Kwaku the Traveller",
+    year: "2022",
+    duration: 212,
+    url: "https://youtu.be/GIDiI5kyBDQ?si=IJj3i-mGpFXrjaGB",
+    explicit: true
   },
   {
     songID: "SID-1144",
-    artistId : "AID-0331",
-    title : "Oil In My Head",
-    year : "2023",
-    duration : 185,
-    url : "https://youtu.be/uwojeKVV7Dk?si=-LWYQaGuqE4Ucz55",
-    explicit : true
+    artistId: "AID-0331",
+    title: "Oil In My Head",
+    year: "2023",
+    duration: 185,
+    url: "https://youtu.be/uwojeKVV7Dk?si=-LWYQaGuqE4Ucz55",
+    explicit: true
   },
   {
     songID: "SID-1155",
-    artistId : "AID-0331",
-    title : "45",
-    year : "2022",
-    duration : 200,
-    url : "https://youtu.be/hSib7E4Ctcg?si=heHqfVEOztfG2y4M",
-    explicit : true
+    artistId: "AID-0331",
+    title: "45",
+    year: "2022",
+    duration: 200,
+    url: "https://youtu.be/hSib7E4Ctcg?si=heHqfVEOztfG2y4M",
+    explicit: true
   },
   {
     songID: "SID-1166",
-    artistId : "AID-0331",
-    title : "Second Sermon (Remix)",
-    year : "2022",
-    duration : 199,
-    url : "https://youtu.be/cPf8LT2uNbs?si=vkFTZjeCPwGy2eeO",
-    explicit : true
+    artistId: "AID-0331",
+    title: "Second Sermon (Remix)",
+    year: "2022",
+    duration: 199,
+    url: "https://youtu.be/cPf8LT2uNbs?si=vkFTZjeCPwGy2eeO",
+    explicit: true
   },
   {
     songID: "SID-1177",
-    artistId : "AID-0331",
-    title : "Konongo Zongo",
-    year : "2022",
-    duration : 185,
-    url : "https://youtu.be/BtJQX53Syec?si=sSDtNdj5jxecPq2n",
-    explicit : true
+    artistId: "AID-0331",
+    title: "Konongo Zongo",
+    year: "2022",
+    duration: 185,
+    url: "https://youtu.be/BtJQX53Syec?si=sSDtNdj5jxecPq2n",
+    explicit: true
   },
   //Kwesi Arthur
   {
     songID: "SID-2211",
-    artistId : "AID-0635",
-    title : "Pain Interlude",
-    year : "2023",
-    duration : 185,
-    url : "https://youtu.be/Ar2oStMGGFE?si=-YRceF0CNJACSJ8F",
-    explicit : true
+    artistId: "AID-0635",
+    title: "Pain Interlude",
+    year: "2023",
+    duration: 185,
+    url: "https://youtu.be/Ar2oStMGGFE?si=-YRceF0CNJACSJ8F",
+    explicit: true
   },
   {
     songID: "SID-2222",
-    artistId : "AID-0635",
-    title : "Nirvana",
-    year : "2022",
-    duration : 149,
-    url : "https://youtu.be/E1YDr0PYg34?si=3EiiM9Yw_bSqnThe",
-    explicit : true
+    artistId: "AID-0635",
+    title: "Nirvana",
+    year: "2022",
+    duration: 149,
+    url: "https://youtu.be/E1YDr0PYg34?si=3EiiM9Yw_bSqnThe",
+    explicit: true
   },
   {
     songID: "SID-2233",
-    artistId : "AID-0635",
-    title : "Celebrate",
-    year : "2022",
-    duration : 147,
-    url : "https://youtu.be/j_iKE6ffJ0c?si=ewHW9c7mwXmslkZ8",
-    explicit : true
+    artistId: "AID-0635",
+    title: "Celebrate",
+    year: "2022",
+    duration: 147,
+    url: "https://youtu.be/j_iKE6ffJ0c?si=ewHW9c7mwXmslkZ8",
+    explicit: true
   },
   {
     songID: "SID-2244",
-    artistId : "AID-0635",
-    title : "Thoughts of King Arthur 4",
-    year : "2019",
-    duration : 359,
-    url : "https://youtu.be/WkF2LreVWE8?si=GY17kvUKO6Kd09Zf",
-    explicit : true
+    artistId: "AID-0635",
+    title: "Thoughts of King Arthur 4",
+    year: "2019",
+    duration: 359,
+    url: "https://youtu.be/WkF2LreVWE8?si=GY17kvUKO6Kd09Zf",
+    explicit: true
   },
   {
     songID: "SID-2255",
-    artistId : "AID-0635",
-    title : "Different",
-    year : "2022",
-    duration : 120,
-    url : "https://youtu.be/8UJViveCkU0?si=mXz8BoY6yXAJM1BZ",
-    explicit : true
+    artistId: "AID-0635",
+    title: "Different",
+    year: "2022",
+    duration: 120,
+    url: "https://youtu.be/8UJViveCkU0?si=mXz8BoY6yXAJM1BZ",
+    explicit: true
   },
   {
     songID: "SID-2266",
-    artistId : "AID-0635",
-    title : "Live From 233",
-    year : "2020",
-    duration : 164,
-    url : "https://youtu.be/dXDp8kGfA7s?si=WxfwBR9B8oQoIGJW",
-    explicit : true
+    artistId: "AID-0635",
+    title: "Live From 233",
+    year: "2020",
+    duration: 164,
+    url: "https://youtu.be/dXDp8kGfA7s?si=WxfwBR9B8oQoIGJW",
+    explicit: true
   },
   {
     songID: "SID-2277",
-    artistId : "AID-0635",
-    title : "Winning",
-    year : "2021",
-    duration : 194,
-    url : "https://youtu.be/WHVSy7jZ6uI?si=YWgn4VSqalkFo3K8",
-    explicit : true
+    artistId: "AID-0635",
+    title: "Winning",
+    year: "2021",
+    duration: 194,
+    url: "https://youtu.be/WHVSy7jZ6uI?si=YWgn4VSqalkFo3K8",
+    explicit: true
   }
 ];
